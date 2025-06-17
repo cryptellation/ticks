d09e09e4560eb7570d36885517f18e6430c94952
Louis FRADIN <louis.fradin@gmail.com>
2025-06-17 14:38:38 +0200

    feat: implement wfclient for ticks service

--- pkg/clients/wfclient.go
+++ pkg/clients/wfclient.go
@@ -1,7 +1,23 @@
 package clients
 
+import (
+	"github.com/cryptellation/ticks/api"
+	"go.temporal.io/sdk/workflow"
+)
+
 // WfClient is a client for the cryptellation ticks service from a workflow perspective.
 type WfClient interface {
+	// ListenToTicks listens to ticks from the given exchange and pair.
+	ListenToTicks(
+		ctx workflow.Context,
+		params api.RegisterForTicksListeningWorkflowParams,
+	) (api.RegisterForTicksListeningWorkflowResults, error)
+
+	// StopListeningToTicks unregisters a callback workflow from ticks for a given exchange and pair.
+	StopListeningToTicks(
+		ctx workflow.Context,
+		params api.UnregisterFromTicksListeningWorkflowParams,
+	) (api.UnregisterFromTicksListeningWorkflowResults, error)
 }
 
 type wfClient struct{}
@@ -11,4 +27,46 @@ type wfClient struct{}
 // It is not used to call workflows from outside the workflow environment.
 func NewWfClient() WfClient {
 	return wfClient{}
+}
+
+// ListenToTicks listens to ticks from the given exchange and pair.
+func (c wfClient) ListenToTicks(
+	ctx workflow.Context,
+	params api.RegisterForTicksListeningWorkflowParams,
+) (api.RegisterForTicksListeningWorkflowResults, error) {
+	// Set options
+	childWorkflowOptions := workflow.ChildWorkflowOptions{
+		TaskQueue: api.WorkerTaskQueueName,
+	}
+	ctx = workflow.WithChildOptions(ctx, childWorkflowOptions)
+
+	// Execute child workflow
+	var res api.RegisterForTicksListeningWorkflowResults
+	err := workflow.ExecuteChildWorkflow(ctx, api.RegisterForTicksListeningWorkflowName, params).Get(ctx, &res)
+	if err != nil {
+		return api.RegisterForTicksListeningWorkflowResults{}, err
+	}
+
+	return res, nil
+}
+
+// StopListeningToTicks unregisters a callback workflow from ticks for a given exchange and pair.
+func (c wfClient) StopListeningToTicks(
+	ctx workflow.Context,
+	params api.UnregisterFromTicksListeningWorkflowParams,
+) (api.UnregisterFromTicksListeningWorkflowResults, error) {
+	// Set options
+	childWorkflowOptions := workflow.ChildWorkflowOptions{
+		TaskQueue: api.WorkerTaskQueueName,
+	}
+	ctx = workflow.WithChildOptions(ctx, childWorkflowOptions)
+
+	// Execute child workflow
+	var res api.UnregisterFromTicksListeningWorkflowResults
+	err := workflow.ExecuteChildWorkflow(ctx, api.UnregisterFromTicksListeningWorkflowName, params).Get(ctx, &res)
+	if err != nil {
+		return api.UnregisterFromTicksListeningWorkflowResults{}, err
+	}
+
+	return res, nil
 }
